b8528411df0c799d19b09dc19fafe0d7cbfd66c5
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-06-09 22:35:46 +0530

    initial commit

---
+++ authX-backend/src/main/java/com/security/authX_backend/AuthXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.security.authX_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthXBackendApplication.class, args);
+	}
+
+}
---
+++ authX-backend/src/test/java/com/security/authX_backend/AuthXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.authX_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
