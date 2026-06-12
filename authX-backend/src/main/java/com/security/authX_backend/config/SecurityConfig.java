c723b5ec208d5fede32bc3a0d355146fcf0f499f
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-06-13 00:01:36 +0530

    jwtAuthFilter

--- authX-backend/src/main/java/com/security/authX_backend/config/SecurityConfig.java
+++ authX-backend/src/main/java/com/security/authX_backend/config/SecurityConfig.java
@@ -1,32 +1,64 @@
 package com.security.authX_backend.config;
 
+import com.security.authX_backend.entity.User;
+import com.security.authX_backend.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
     {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        httpSecurity
-                .authorizeHttpRequests(authorizeHttpRequests -> {
-                    authorizeHttpRequests.requestMatchers("/api/v1/auth/register").permitAll()
-                            .requestMatchers("/api/v1/auth/login").permitAll()
-                            .anyRequest().authenticated();
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                        .cors(Customizer.withDefaults())
+                                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                    .authorizeHttpRequests(authorizeHttpRequests -> {
+                                        authorizeHttpRequests.requestMatchers("/api/v1/auth/register").permitAll()
+                                                .requestMatchers("/api/v1/auth/login").permitAll()
+                                                .anyRequest().authenticated();
 
 
-                })
-                .httpBasic(Customizer.withDefaults());
+                                    })
+                .exceptionHandling(ex-> ex.authenticationEntryPoint(((request, response, authException) ->
+                {
+                    //error message
+//                    e.printStackTrace();
+                    response.setStatus(401);
+                    response.setContentType("application/json");
+                    String message = authException.getMessage();
+                    String error = (String) request.getAttribute("error");
+                    if (error != null) {
+                        message = error;
+                    }
+                    Map<String, Object> errorMap = Map.of("message", message, "statusCode",404);
+//                    var apiError = ApiError.of(HttpStatus.UNAUTHORIZED.value(), "Unauthorized Access", message, request.getRequestURI(), true);
+                    var objectMapper = new ObjectMapper();
+                    response.getWriter().write(objectMapper.writeValueAsString(errorMap));
+                })))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
---
+++ authX-backend/src/main/java/com/security/authX_backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,109 @@
+package com.security.authX_backend.security;
+
+import com.security.authX_backend.repository.UserRepository;
+import io.jsonwebtoken.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final  JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+        if(header!=null && header.startsWith("Bearer"))
+        {
+            // Extract token and validate the authentication and then set inside security context
+
+
+            try{
+
+                String  token = header.substring(7);
+                if(!jwtService.isAccessToken(token))
+                {
+                    filterChain.doFilter(request,response);
+                    return;
+                }
+
+                Jws<Claims> parse = jwtService.parse(token);
+                Claims payload = parse.getPayload();
+                UUID userId = UUID.fromString(payload.getSubject());
+                userRepository.findById(userId)
+                        .ifPresent(user -> {
+                            if(!user.isEnable())
+                            {
+                                try {
+                                    filterChain.doFilter(request,response);
+                                } catch (IOException | ServletException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            if(user.isEnabled())
+                            {
+                                List<GrantedAuthority> authorities = user.getRoles() == null ? List.of() : user.getRoles().stream()
+                                        .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+                                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                                        user.getEmail(),
+                                        null,
+                                        authorities
+                                );
+                                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                                if(SecurityContextHolder.getContext().getAuthentication() == null)
+                                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                            }
+
+                        });
+
+
+
+            }
+            catch (ExpiredJwtException e)
+            {
+                e.printStackTrace();
+            }
+            catch (MalformedJwtException e)
+            {
+                e.printStackTrace();
+
+            }
+            catch (JwtException e)
+            {
+                e.printStackTrace();
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        }
+
+        filterChain.doFilter(request,response);
+
+    }
+}
